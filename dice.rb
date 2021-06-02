4dcbf9c3629aeb1cd67956a588d6093df4e30d13
TAKANO Mitsuhiro <takano32@gmail.com>
2021-06-02 22:59:29 +0900

    うんち、ちんこ、まんこ、おまんこ、おちんこ、おちんちん の実装

--- dice.rb
+++ dice.rb
@@ -41,23 +41,51 @@ class Dice
     self.んこ確率('うんこ', rolls)
   end
 
-  def うんち
+  def うんち?(roll)
+    self.んこ?('うんち', roll)
+  end
+
+  def self.うんち確率(rolls)
+    self.んこ確率('うんち', rolls)
   end
   
-  def ちんこ
+  def ちんこ?
+    self.んこ?('ちんこ', roll)
   end
 
-  def まんこ
+  def self.ちんこ確率(rolls)
+    self.んこ確率('ちんこ', rolls)
   end
 
-  def おまんこ
+  def まんこ?
+    self.んこ?('まんこ', roll)
   end
 
-  def おちんこ
+  def self.まんこ確率(rolls)
+    self.んこ確率('まんこ', rolls)
   end
 
-  def おちんちん
+  def おまんこ?
+    self.んこ?('おまんこ', roll)
   end
 
+  def self.おまんこ確率(rolls)
+    self.んこ確率('おまんこ', rolls)
+  end
 
+  def おちんこ?
+    self.んこ?('おちんこ', roll)
+  end
+
+  def self.おちんこ確率(rolls)
+    self.んこ確率('おちんこ', rolls)
+  end
+
+  def おちんちん?
+    self.んこ?('おちんちん', roll)
+  end
+
+  def self.おちんちん確率(rolls)
+    self.んこ確率('おちんちん', rolls)
+  end
 end
