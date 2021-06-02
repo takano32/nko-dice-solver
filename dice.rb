35598568ca2179f58398d56b4039765a90bbe696
TAKANO Mitsuhiro <takano32@gmail.com>
2021-06-02 22:54:25 +0900

    んこ? と んこ確率 を発明

--- dice.rb
+++ dice.rb
@@ -13,23 +13,32 @@ class Dice
     @cells.repeated_permutation(n).to_a
   end
 
-  def self.うんこ?(roll)
-    return false unless roll.include?('う')
-    return false unless roll.include?('ん')
-    return false unless roll.include?('こ')
+
+  def self.んこ?(word, roll)
+    word.each_char do |ch|
+      return false unless roll.include?(ch)
+    end
     return true
   end
 
-  def self.うんこ確率(rolls)
+  def self.んこ確率(word, rolls)
     numerator = 0
     rolls.each do |roll|
-      if Dice.うんこ?(roll)
+      if Dice.send("#{word}?", roll)
         numerator += 1
       end
     end
 
     denominator = rolls.size
     Rational(numerator, denominator)
+  end
+
+  def self.うんこ?(roll)
+    self.んこ?('うんこ', roll)
+  end
+
+  def self.うんこ確率(rolls)
+    self.んこ確率('うんこ', rolls)
   end
 
   def うんち
